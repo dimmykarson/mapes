15fb9585cba2939ae0f50ed66c3ded19c7032fb1
Dimmy Magalhães <dimmyk@gmail.com>
2019-01-26 12:42:25 -0200

    Job test resources

---
+++ resources/estrutura consulta exame.sql
@@ -0,0 +1,30 @@
+﻿
+
+DROP TABLE IF EXISTS consulta CASCADE;
+CREATE TABLE consulta
+(
+  numero_guia_consulta integer,
+  cod_medico integer not null,
+  nome_medico VARCHAR,
+  data_consulta DATE,
+  valor_consulta NUMERIC,
+  CONSTRAINT consulta_pkey PRIMARY KEY (numero_guia_consulta)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE consulta OWNER TO postgres;
+
+DROP TABLE IF EXISTS exame;
+CREATE TABLE exame
+(
+  cod_exame integer,
+  numero_guia_consulta integer,
+  valor_exame NUMERIC,
+  CONSTRAINT exame_pkey PRIMARY KEY (cod_exame,numero_guia_consulta),
+  CONSTRAINT numero_guia_consulta_fkey FOREIGN KEY (numero_guia_consulta) REFERENCES CONSULTA (numero_guia_consulta)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE exame OWNER TO postgres;
