7bc6e794d0b400930bac1591ebadcb5dfce14f86
Dimmy Magalhães <dimmyk@gmail.com>
2019-01-26 20:32:35 -0200

    Previous function, without pagination and bootstrap

--- exams/reports/urls.py
+++ exams/reports/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-
-from reports.views import *
+from .views import *
 
 urlpatterns = [
     path('', index, name='report'),
-    path('doctor_production/', DoctorProductionJSON.as_view(),
-    	name='json_doctor_production'),
-
+    path('doctor_production/', DoctorProductionJSON.as_view(), name='json_doctor_production'),
  ]
--- exams/reports/views.py
+++ exams/reports/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import  UserCreationForm
 from django.http import HttpResponse
-from .models import *
+from reports.models import *
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from django.utils.html import escape
 
@@ -13,13 +13,13 @@ def index(request):
 
 
 class DoctorProductionJSON(BaseDatatableView):
-	model = Consulta
 	columns = ['nome_medico', 'numero_guia_consulta', 'data_consulta', 'valor_consulta']
 	order_columns = ['nome_medico']
 	max_display_length = 50
 
-	def render_column(self, row, column):
-		return super(ProducaoMedicaJson, self).render_column(row, column)
+	def get_initial_queryset(self):
+		print("Teste")
+		return Consulta.objects.all()
 
 	def filter_queryset(self, qs):
 		return qs
