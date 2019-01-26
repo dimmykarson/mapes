18f4c2f0ddcf6b2772405e46a393d0778e100ab7
Dimmy Magalhães <dimmyk@gmail.com>
2019-01-26 13:41:37 -0200

    Import dataset

--- exams/exams/settings.py
+++ exams/exams/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reports.apps.ReportsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -73,11 +74,14 @@ WSGI_APPLICATION = 'exams.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mapes_application',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432', # 8000 is default
     }
 }
 
--- exams/reports/models.py
+++ exams/reports/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from postgres_copy import CopyManager
 
-# Create your models here.
+class Consulta(models.Model):
+    numero_guia_consulta = models.IntegerField(primary_key=True)
+    cod_medico = models.IntegerField()
+    nome_medico = models.CharField(max_length=5000, blank=True, null=True)
+    data_consulta = models.DateField(blank=True, null=True)
+    valor_consulta = models.DecimalField(max_digits=1000, decimal_places=2, blank=True, null=True)
+    objects = CopyManager()
+
+    class Meta:
+        managed = False
+        db_table = 'consulta'
+
+
+class Exame(models.Model):
+    cod_exame = models.IntegerField(primary_key=True)
+    numero_guia_consulta = models.ForeignKey(Consulta, models.DO_NOTHING, db_column='numero_guia_consulta')
+    valor_exame = models.DecimalField(max_digits=1000, decimal_places=2, blank=True, null=True)
+    objects = CopyManager()
+
+    class Meta:
+        managed = False
+        db_table = 'exame'
+        unique_together = (('cod_exame', 'numero_guia_consulta'),)
