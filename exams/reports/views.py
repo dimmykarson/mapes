15278009c36c5e1ea65782ff1acb57bbf1dd6830
Dimmy Magalhães <dimmyk@gmail.com>
2019-01-27 01:04:26 -0200

    Search form

--- exams/reports/views.py
+++ exams/reports/views.py
@@ -8,21 +8,22 @@ from django.db.models.functions import Coalesce
 import locale
 
 def index(request):
-	consultas = Consulta.objects.all()
-	return render(request, 'reports/index.html', {'consultas':consultas})
+	return render(request, 'reports/index.html', {})
 
+def load_doctors(request):
+    doctors = Consulta.objects.distinct('cod_medico', 'nome_medico').order_by('nome_medico')
+    return render(request, 'selects/doctors.html', {'doctors': doctors})
 
 
 class DoctorProductionJSON(BaseDatatableView):
 	columns = ['nome_medico', 'numero_guia_consulta', 'data_consulta', 'valor_consulta', 'gasto', 'qt_exames']
-	order_columns = ['nome_medico']
+	order_columns = ['gasto', 'nome_medico', 'numero_guia_consulta', 'data_consulta', 'valor_consulta', 'qt_exames']
 	max_display_length = 50
 	model = Consulta
 
 	def filter_queryset(self, qs):
 		qs = Consulta.objects\
-		.annotate(gasto=Coalesce(Sum('exame__valor_exame'), 0), qt_exames=Count('exame'))\
-		.order_by('-gasto')
+		.annotate(gasto=Coalesce(Sum('exame__valor_exame'), 0), qt_exames=Count('exame'))
 		return qs
 
 	def prepare_results(self, qs):
